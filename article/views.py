37698ba8993f135ea50f49dd25fd06bcd0f6c9e5
Vlad Babin <Vlad_Babin_2013@mail.ru>
2016-02-25 21:26:38 +0200

    cookies

--- article/views.py
+++ article/views.py
@@ -32,29 +32,38 @@ def articles(request):
     return render_to_response('articles.html', {'articles': Article.objects.all()})
 
 def article (request, article_id):
-    comment_form = CommentForm
-    args = {}
-    args.update(csrf(request))
-    args['article'] = Article.objects.get(id=article_id)
-    args['comment'] = Comments.objects.filter(comments_article_id=article_id)
-    args['form'] = comment_form
-    return render_to_response('article.html', args)
+    article = Article.objects.get(id=article_id)
+    comments = Comments.objects.filter(comments_article_id=article_id)
+    form = CommentForm()
+    return render(request, 'article.html', {
+        'article': article,
+        'comments': comments,
+        'form': form
+    })
 
 def addlike (request, article_id):
     try:
-        article = Article.objects.get(id=article_id)
-        article.article_likes +=1
-        article.save()
+        if article_id in request.COOKIES:
+            redirect('/')
+        else :
+            article = Article.objects.get(id=article_id)
+            article.article_likes +=1
+            article.save()
+            responce = redirect('/')
+            responce.set_cookie(article_id, "test")
+            return responce
     except ObjectDoesNotExist:
         raise Http404
     return redirect('/')
 
-def addcomment (request, article_id):
-    if request.POST:
+def addcomment(request, article_id):
+    if request.POST and ("pause" not in request.session ):
         form = CommentForm(request.POST)
         if form.is_valid():
             comment = form.save(commit=False)
             comment.comments_article = Article.objects.get(id=article_id)
             form.save()
+            request.session.set_expiry(60)
+            request.session['pause'] = True
     return redirect('/articles/get/%s' % article_id)
 
